ff3f6d35e3b4f6f85566c1f913be98ec4b3003f9
Your Name <never_toni@abv.bg>
2018-02-14 22:41:20 +0200

    ubre

--- script.js
+++ script.js
@@ -1,59 +1,62 @@
-/*document.getElementById("asdf").addEventListener("onmouseover", changeSize(1.5, 2.0, 0.1)).addEventListener("onmouseout", changeSize(2.0, 1.5, -0.1));
-
-function changeSize(begin, end, step) {
-    var elem = document.getElementById("asdf");
-    var inc = begin;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (inc >= end) {
-            clearInterval(id);
-        } else {
-            inc += step;
-            elem.style.fontSize = inc + "em"; 
-        }
-    }
-}*/
 $(document).ready(function(){
-    var audioElement = document.createElement('audio');
-    audioElement.setAttribute('src', 'click.mp3');
+    particlesJS.load('particles-js', 'particles.json',
+        function () {
+            console.log('particles.json loaded...')
+        })
+
+    var clickSound = document.createElement('audio');
+    clickSound.setAttribute('src', 'click.mp3');
+    clickSound.volume = 0.4;
+
+    document.getElementById("particles-js").addEventListener("onload", changeColor());
+
+    $(".button").click(function() {
+        clickSound.play();
+    });
 
     $("#leftb").click(function() {
         $('html,body').animate({
             scrollTop: $("#blurry").offset().top},'slow');
-        audioElement.play();
     });
 
-    /*$(".landing > h1, .landing > p, .landing > div").hide();
+    $("#rightb").click(function() {
+        alert("Come back when you're ready");
+    });
+
+    $(".landing > h1, .landing > p, .landing > div").hide();
     $(".landing > h1").fadeIn(1500, function() {
         $(".landing > p, .landing > div").fadeIn(1500);
-    });*/
-    document.getElementById("particles-js").addEventListener("onload", changeColor());
+    });
 
+    $("#leftb, #rightb").mouseenter(function() {
+        $(this).animate({
+            fontSize: "2.0em"
+        });
+    });
+
+    $("#leftb, #rightb").mouseleave(function() {
+        $(this).animate({
+            fontSize: "1.7em"
+        });
+    });
+
+    $("#left").mouseenter(function() {
+        $("#left h1").animate({
+            fontSize: "70px",
+        }, 'slow');
+    });
+
+    $("#left").mouseleave(function() {
+        $("#left h1").animate({
+            fontSize: "60px"
+        }, 'slow');
+    });
 });
-var buttons = document.getElementsByClassName("button");
-
-//buttons[0].addEventListener("mouseover", function() {changeSize(1.5, 2.0, 0.1, 0);});
-//buttons[0].addEventListener("mouseout", function() {changeSize(2.0, 1.5, 0.1, 0);});
-
 
 function changeColor() {
     var elem = document.getElementById("particles-js");
-    var red1 = 65, red2 = 50, rcount;
-    var green1 = 105, green2 = 205, gcount;
-    var blue1 = 225, blue2 = 50, bcount;
+    var red1 = 65, red2 = 50;
+    var green1 = 105, green2 = 205;
+    var blue1 = 225, blue2 = 50;
     elem.style.background = "linear-gradient(to right, rgb(" + red1 + ", " + green1 + ", " + blue1 + "), rgb(" + red2 + ", " + green2 + ", " + blue2 + "))";
 }
-/*function changeSize(begin, end, step, i) {
-    var buttons = document.getElementsByClassName("button");
-    var elem = buttons[i];
-    var inc = begin;
-    var id = setInterval(frame, 30);
-    function frame() {
-        if (inc >= end) {
-            clearInterval(id);
-        } else {
-            inc += step;
-            elem.style.fontSize = inc + "em"; 
-        }
-    }
-}*/
